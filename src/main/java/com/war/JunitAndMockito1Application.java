75cc4cec3933b500a710a72f5ffd621df7e8c06d
Nikita Jawali <nikitajawali06@gmail.com>
2022-11-07 23:12:45 +0530

    added

---
+++ src/main/java/com/war/JunitAndMockito1Application.java
@@ -0,0 +1,13 @@
+package com.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitAndMockito1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitAndMockito1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/war/JunitAndMockito1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitAndMockito1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
